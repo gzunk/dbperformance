fb9a6673c2d1dea19d108b2c1e36af045ef972db
Alasdair Thomson <alasdair@gzunk.com>
2017-10-06 22:46:59 +0100

    h2

--- src/main/java/com/gzunk/dbperformance/ApplicationMain.java
+++ src/main/java/com/gzunk/dbperformance/ApplicationMain.java
@@ -1,5 +1,6 @@
 package com.gzunk.dbperformance;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.postgresql.ds.PGSimpleDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +15,7 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -25,14 +27,23 @@ public class ApplicationMain {
     private static final Logger LOG = LoggerFactory.getLogger(ApplicationMain.class);
     private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+//    @Bean
+//    DataSource dataSource() {
+//        PGSimpleDataSource ds = new PGSimpleDataSource();
+//        ds.setServerName("postgres.dunblane");
+//        ds.setDatabaseName("movies");
+//        ds.setUser("postgres");
+//        ds.setPassword("postgres");
+//
+//        return ds;
+//    }
+
     @Bean
     DataSource dataSource() {
-        PGSimpleDataSource ds = new PGSimpleDataSource();
-        ds.setServerName("postgres.dunblane");
-        ds.setDatabaseName("movies");
-        ds.setUser("postgres");
-        ds.setPassword("postgres");
-
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setURL("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+        ds.setUser("alasdair");
+        ds.setPassword("thomson");
         return ds;
     }
 
@@ -47,29 +58,37 @@ public class ApplicationMain {
 
     @PostConstruct
     public void runQuery() {
-        LOG.info("Run a query");
 
-        LOG.info("Count: {}", jdbcOperations().queryForObject("select count (*) from table2", Integer.class));
-        clearData();
-        LOG.info("Count: {}", jdbcOperations().queryForObject("select count (*) from table2", Integer.class));
-        // insertRandomData(1000000);
-        // selectData();
+        JdbcOperations main = jdbcOperations();
 
-        copyDataBatched();
-        // copyData();
+        main.execute("CREATE TABLE TABLE1 (column_1 varchar(20), column_2 varchar(20))");
+        main.execute("CREATE TABLE TABLE2 (column_1 varchar(20), column_2 varchar(20))");
+        LOG.info("Create Tables");
 
-        LOG.info("Count: {}", jdbcOperations().queryForObject("select count (*) from table2", Integer.class));
+        LOG.info("Count 1: {}", main.queryForObject("select count(*) from TABLE1", Integer.class));
+        LOG.info("Count 2: {}", main.queryForObject("select count(*) from TABLE2", Integer.class));
+
+        LOG.info("Populate Table 1");
+        insertRandomData(100, main);
+
+        LOG.info("Count 1: {}", main.queryForObject("select count (*) from table1", Integer.class));
+        LOG.info("Count 2: {}", main.queryForObject("select count (*) from table2", Integer.class));
+
+        main.query("SELECT column_1 FROM TABLE1", (ResultSet rs, int i) -> rs.getString(1)).forEach(LOG::info);
+
+        LOG.info("Transferring Data");
+        MyRowCallbackHandler callbackHandler = new MyRowCallbackHandler(jdbcOperations());
+        main.query("select * from table1", callbackHandler);
+        callbackHandler.saveResults();
+
+        LOG.info("Count 1: {}", main.queryForObject("select count (*) from table1", Integer.class));
+        LOG.info("Count 2: {}", main.queryForObject("select count (*) from table2", Integer.class));
 
     }
 
     private void clearData() {
         // jdbcOperations().update("DELETE FROM table1");
         jdbcOperations().update("DELETE FROM table2");
-    }
-
-    @SuppressWarnings("unused")
-    private void selectData() {
-        jdbcOperations().query("SELECT column_1 FROM table1", (ResultSet rs, int i) -> rs.getString(1)).forEach(LOG::info);
     }
 
     @SuppressWarnings("unused")
@@ -95,9 +114,10 @@ public class ApplicationMain {
 
     private class MyRowCallbackHandler implements RowCallbackHandler {
 
-        private static final int batchSize = 1000;
+        private static final int batchSize = 75;
         private List<Object[]> batchArgs;
         private JdbcOperations jdbcOperations;
+        String columnPlaceholders;
 
         private MyRowCallbackHandler(JdbcOperations jdbcOperations) {
             this.jdbcOperations = jdbcOperations;
@@ -109,48 +129,39 @@ public class ApplicationMain {
 
             int columnCount = rs.getMetaData().getColumnCount();
             Object[] results = new Object[columnCount];
-            String columnPlaceholders = String.join(",", Collections.nCopies(columnCount, "?"));
+            columnPlaceholders = String.join(",", Collections.nCopies(columnCount, "?"));
 
             for (int j = 0; j < columnCount; j++) {
                 results[j] = rs.getObject(j+1);
             }
             batchArgs.add(results);
 
-            if (batchArgs.size() >= batchSize || rs.isLast()) {
-                LOG.info("Writing Data");
-                jdbcOperations.batchUpdate("insert into table2 values (" + columnPlaceholders + ")", batchArgs);
-                batchArgs = new ArrayList<>();
+            if (batchArgs.size() >= batchSize ) {
+                saveResults();
             }
+        }
+
+        public void saveResults() {
+            LOG.info("Writing Data");
+            jdbcOperations.batchUpdate("insert into table2 values (" + columnPlaceholders + ")", batchArgs);
+            batchArgs = new ArrayList<>();
         }
     }
 
-    private void copyDataBatched() {
-
-        LOG.info("Transferring Data");
-        jdbcOperations().query("select * from table1", new MyRowCallbackHandler(jdbcOperations()));
-
-    }
-
     @SuppressWarnings("unused")
-    private void insertRandomData(final int count) {
+    private void insertRandomData(final int count, JdbcOperations main) {
 
         Random rng = new Random();
 
         LOG.info("Inserting Data");
-
-        JdbcOperations main = jdbcOperations();
         for (int i = 0; i < count; i++) {
             Object[] args = {
                     generateString(rng),
                     generateString(rng),
-                    generateInt(rng),
-                    generateString(rng),
-                    generateString(rng),
-                    generateInt(rng),
-                    generateString(rng)
             };
 
-            main.update("INSERT INTO table1 VALUES (?,?,?,?,?,?,?)", args);
+            // main.update("INSERT INTO table1 VALUES (?,?,?,?,?,?,?)", args);
+            main.update("INSERT INTO TABLE1 VALUES (?,?)", args);
         }
 
         LOG.info("inserted {} rows", count);
